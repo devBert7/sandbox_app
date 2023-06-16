749c549880ea26d8fe23299e7961e73d23fcad62
Robert Thiel <33242830+devBert7@users.noreply.github.com>
2023-06-16 05:39:33 -0700

    Expense Date Component Break-Away
    Extracted the date component from ExpenseItem and created its own component and CSS.

---
+++ src/components/ExpenseDate.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ExpenseDate = ({date}) => {
+    const month = date.toLocaleString('en-US', {month: 'long'}),
+        day = date.toLocaleString('en-US', {day: '2-digit'}),
+        year = date.getFullYear();
+    
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__year">{year}</div>
+        </div>
+    );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,10 @@
 import "./expenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = ({date, title, amount}) => {
-    const month = date.toLocaleString('en-US', {month: 'long'}),
-        day = date.toLocaleString('en-US', {day: '2-digit'}),
-        year = date.getFullYear();
-    
     return (
         <div className="expense-item" >
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-            {/*<div>
-                {date.toISOString()}
-            </div>*/}
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
