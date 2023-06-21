22421be39f2124bbcc444b29130cee83f397bc50
Robert Thiel <33242830+devBert7@users.noreply.github.com>
2023-06-21 08:30:03 -0700

    Prepared App to render stateful Expenses component list, updated by an added expense dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,50 @@
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
+import {useState} from "react";
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2023, 6, 13)
+    },
+    {
+        id: 'e2',
+        title: "Steak",
+        amount: 42.39,
+        date: new Date(2023, 6, 13)
+    },
+    {
+        id: 'e3',
+        title: "Paper Towels",
+        amount: 12.38,
+        date: new Date(2023, 6, 13)
+    },
+    {
+        id: 'e4',
+        title: "Grapes",
+        amount: 5.65,
+        date: new Date(2023, 6, 13)
+    }
+];
 
 const App = () => {
-    const expenses = [
-        {
-            id: 'e1',
-            title: "Car Insurance",
-            amount: 294.67,
-            date: new Date(2023, 6, 13)
-        },
-        {
-            id: 'e2',
-            title: "Steak",
-            amount: 42.39,
-            date: new Date(2023, 6, 13)
-        },
-        {
-            id: 'e3',
-            title: "Paper Towels",
-            amount: 12.38,
-            date: new Date(2023, 6, 13)
-        },
-        {
-            id: 'e4',
-            title: "Grapes",
-            amount: 5.65,
-            date: new Date(2023, 6, 13)
-        }
-    ];
+    const [expensesArr, setExpensesArr] = useState(DUMMY_EXPENSES);
     
     const addExpenseHandler = (expense) => {
-        expenses.push(expense);
-        console.log(expenses);
+        setExpensesArr((previousExpenses) => {
+            return [
+                expense,
+                ...previousExpenses
+            ];
+        });
     };
     
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses expenses={expenses} />
+            <Expenses expenses={expensesArr} />
         </div>
     );
 };
--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -13,26 +13,16 @@ const Expenses = ({expenses}) => {
     return (
         <Card>
             <ExpensesFilter onFilterChange={filterChangeHandler} selectedYear={filteredYear} />
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+            {
+                expenses.map((expenses) => {
+                        return <ExpenseItem
+                            title={expenses.title}
+                            amount={expenses.amount}
+                            date={expenses.date}
+                        />
+                    }
+                )
+            }
         </Card>
     );
 };
